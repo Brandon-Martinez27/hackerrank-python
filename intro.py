4c4c0a8884c3468dd03ef07ae5a33e36b3dd2e49
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-03-31 22:42:36 -0500

    finished intro module, started data types modele, second star

--- intro.py
+++ intro.py
@@ -62,3 +62,19 @@ count = 0
 for i in range(1,n+1):
     print(count**2)
     count += 1
+
+# Print Function
+'''
+The included code stub will read an integer, n, from STDIN.
+Without using any string methods, try to print the following:
+123...n
+Note that "" represents the consecutive values in between.
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    "".join([str(i) for i in range(1,n+1)])
+# OR 
+    for i in range(1, n+1):
+     print(i, end='')
