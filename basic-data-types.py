245e8029339445e56d2fe901e306d397a2046f6b
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-04-01 20:41:46 -0500

    worked with nested lists

--- basic-data-types.py
+++ basic-data-types.py
@@ -14,3 +14,46 @@ if __name__ == '__main__':
     n = int(input())
     
     print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
+
+# Find the Runner-up Score!
+'''
+Given the participants' score sheet for your University Sports Day, 
+you are required to find the runner-up score. You are given scores. 
+Store them in a list and find the score of the runner-up.
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    a = list(set(arr))
+    a.sort(reverse=True)
+    print(a[1])
+
+# Nested Lists
+'''
+Given the names and grades for each student in a class of N students, 
+store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+Note: If there are multiple students with the second lowest grade, 
+order their names alphabetically and print each name on a new line.
+'''
+
+main_list = []
+second_lowest_names = []
+scores = set()
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        
+        main_list.append([name, score])
+        scores.add(score)
+        
+second_lowest = sorted(scores)[1]
+
+for name, score in main_list:
+    if score == second_lowest:
+        second_lowest_names.append(name)
+
+for name in sorted(second_lowest_names):
+    print(name, end='\n')
