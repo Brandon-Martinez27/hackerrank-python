5d80b2a6ed2b342a921b3ce0d33ca9754f1c6a7b
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-05-05 13:47:41 -0500

    completed lists problem

--- basic-data-types.py
+++ basic-data-types.py
@@ -94,6 +94,22 @@ Iterate through each command in order and perform the corresponding operation
 on your list.
 '''
 
+if __name__ == '__main__':
+    N = int(input())
+    
+    
+    l = []
+    for _ in range(N):
+        s = input().split()
+        cmd = s[0]
+        args = s[1:]
+        if cmd !="print":
+            cmd += "("+ ",".join(args) +")"
+            eval("l."+cmd)
+        else:
+            print(l)
+
+
 # Tuples
 '''
 
